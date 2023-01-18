c58382ecea23595d34a76381d7d7a83d7d4dfc23
feribacsy <feribacsy@gmail.com>
2023-01-18 19:59:11 +0100

    version 0.2

---
+++ csere.js
@@ -0,0 +1,18 @@
+//Kattintásra blue nevű class
+/*element
+  esemény
+  funkcionalitás
+*/
+let isBlue = false;
+
+document.getElementById("element_0").onclick = function(){
+
+    isBlue = !isBlue;
+
+    if(isBlue) {
+        document.getElementById("element_0").classList.add("blue");
+    } else {
+        document.getElementById("element_0").classList.remove("blue");
+    }
+
+}
