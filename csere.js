9d186b7829a3592665c60212d45a7e53d5c4d62b
feribacsy <feribacsy@gmail.com>
2023-01-24 14:47:39 +0100

    Végleges

--- csere.js
+++ csere.js
@@ -14,7 +14,7 @@ document.getElementById("element_0").onclick = function () {
     } else {
         document.getElementById("element_0").classList.remove("blue");
     }
-    console.log(isBlue);
+    //console.log(isBlue);
 }
 
 
@@ -48,6 +48,34 @@ function getRandomNumber(min, max) {
 }
 
 document.getElementById("element_2").onclick = function () {
-    console.log(Math.random()*20)+1;
+    console.log(Math.random() * 20) + 1;
     document.getElementById("element_2").firstElementChild.innerHTML = getRandomNumber(1, 20);
+};
+
+
+document.getElementById("element_3").onclick = function () {
+    document.getElementById("element_3").classList.add("hidden");
+    setTimeout(function () {
+        document.getElementById("element_3").classList.remove("hidden");
+    }, 2000);
+};
+
+//ötödik
+let borderR = "";
+   document.getElementById("element_4").onclick = function () {
+    borderR = !borderR;
+    if (borderR) {
+        let boxes = document.querySelectorAll(".shape");
+        for (box of boxes) {
+            box.classList.add("borderRadius");
+        }
+        
+    } else {
+        boxes = document.querySelectorAll(".shape");
+        for (box of boxes) {
+            box.classList.remove("borderRadius");
+        }
+    }
 }
+
+
